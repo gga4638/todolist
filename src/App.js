0e0e7c84630ec5f236eb64b15951b7947dfe1096
Guk Gyeonga <guk4638@naver.com>
2021-12-21 17:42:48 +0900

    todoList list 추가
    날짜, 갯수 카운트만 구현

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from "react";
+import TodoList from './component/TodoList';
+import TodoHead from './component/TodoHead';
 
 function App() {
-  return (
+    const [items, setItems] = useState([]);
+    const [text, setText] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if(text.length == 0 ){
+            return;
+        }
+        const newItem = {
+            text: text,
+            id: Date.now()
+        };
+        setItems(items.concat(newItem));
+        setText('');
+    }
+
+    const handleChange = (e) => {
+        setText(e.target.value);
+    }
+
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='todo'>
+        <TodoHead count={items.length}/>
+        <TodoList items={items} />
+        <form onSubmit={handleSubmit}>
+            <input
+                onChange={handleChange}
+                value={text}
+            />
+            <button>
+                등록 #{items.length + 1}
+            </button>
+        </form>
+      </div>
     </div>
-  );
+    );
 }
 
 export default App;

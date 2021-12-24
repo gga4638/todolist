fde50675c2ad282a09829a8f197da697d44ab170
국경아 <guk4638@naver.com>
2021-12-24 17:23:03 +0900

    localStorage 사용하여 값 저장
    items를 localStorage에 저장하여 새로고침해도 값이 지워지지 않게 함

--- src/App.js
+++ src/App.js
@@ -3,14 +3,21 @@ import React, {useEffect, useState} from "react";
 import TodoList from './component/TodoList';
 import TodoHead from './component/TodoHead';
 import AddTodoItem from "./component/AddTodoItem";
+import storage from 'redux-persist/lib/storage';
 
 function App() {
-    const [items, setItems] = useState([]);
+    let persistItems = () => JSON.parse(localStorage.getItem('items'));
+    const [items, setItems] = useState(persistItems === null ? [] : persistItems);
     const [todoCount, setTodoCount] = useState(0);
 
     useEffect(() => {
         setTodoCount(items.filter(item => item.check === false).length);
     },[items]);
+
+    useEffect(() => {
+        window.localStorage.setItem('items', JSON.stringify(items));
+        setItems(persistItems);
+    }, [todoCount]);
 
     // 현재 컴포넌트 간에 props를 일일이 전달하고 있다. 어쩐지 뭔가 이상했어...
     // 리액트 컨텍스트를 사용하여 props drilling 현상을 개선하자!
